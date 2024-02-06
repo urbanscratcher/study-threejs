d8d7a77d956fe24c1ce053001ed5b5ee3f7ce80f
Joun <urbanscratcher@gmail.com>
2024-02-06 15:35:37 +0900

    custom geometry

---
+++ threejs-course-2/05-custom-geometry.js
@@ -0,0 +1,114 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { VertexNormalsHelper } from "three/addons/helpers/VertexNormalsHelper.js";
+
+class App {
+  #divContaianer;
+  #renderer;
+  #scene;
+  #camera;
+  #cube;
+
+  constructor() {
+    const divContainer = document.querySelector("#webgl-container");
+    this.#divContaianer = divContainer;
+
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    divContainer.appendChild(renderer.domElement);
+    this.#renderer = renderer;
+
+    const scene = new THREE.Scene();
+    this.#scene = scene;
+
+    this.#setupCamera();
+    this.#setupLight();
+    this.#setupModel();
+    this.#setupControls();
+
+    window.onresize = this.resize.bind(this);
+    this.resize();
+    requestAnimationFrame(this.render.bind(this));
+  }
+
+  #setupCamera() {
+    const width = this.#divContaianer.clientWidth;
+    const height = this.#divContaianer.clientHeight;
+    const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
+    camera.position.z = 2;
+    this.#camera = camera;
+  }
+
+  #setupLight() {
+    const color = 0xffffff;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    this.#scene.add(light);
+  }
+
+  #setupControls() {
+    new OrbitControls(this.#camera, this.#divContaianer);
+  }
+
+  #setupModel() {
+    const rawPositions = [-1, -1, 0, 1, -1, 0, -1, 1, 0, 1, 1, 0];
+    const rawNormals = [0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1];
+    const rawColors = [1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0];
+    const rawUVs = [0, 0, 1, 0, 0, 1, 1, 1];
+
+    const positions = new Float32Array(rawPositions);
+    const normals = new Float32Array(rawNormals);
+    const colors = new Float32Array(rawColors);
+    const uvs = new Float32Array(rawUVs);
+
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
+    geometry.setAttribute("normal", new THREE.BufferAttribute(normals, 3));
+    geometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
+    geometry.setAttribute("uv", new THREE.BufferAttribute(uvs, 2));
+
+    // 반시계 방향으로 삼각형 버텍스 인덱스 지정
+    // 참고로 1개의 사각형은 2개의 삼각형으로 만들어져 있음
+    geometry.setIndex([0, 1, 2, 2, 1, 3]);
+    // geometry.computeVertexNormals();
+
+    const textureLoader = new THREE.TextureLoader();
+    const map = textureLoader.load("./materials/uv_grid_opengl.jpg");
+
+    const material = new THREE.MeshPhongMaterial({
+      color: 0xffffff,
+      // vertexColors: true,
+      map: map,
+    });
+    const box = new THREE.Mesh(geometry, material);
+    this.#scene.add(box);
+
+    const helper = new VertexNormalsHelper(box, 0.1, 0xffff00);
+    this.#scene.add(helper);
+  }
+
+  resize() {
+    const width = this.#divContaianer.clientWidth;
+    const height = this.#divContaianer.clientHeight;
+
+    this.#camera.aspect = width / height;
+    this.#camera.updateProjectionMatrix();
+    this.#renderer.setSize(width, height);
+  }
+
+  // parameter: ms time (for animation)
+  render(time) {
+    this.#renderer.render(this.#scene, this.#camera);
+    this.update(time);
+    requestAnimationFrame(this.render.bind(this));
+  }
+
+  update(time) {
+    time *= 0.001;
+  }
+}
+
+window.onload = () => {
+  new App();
+};
